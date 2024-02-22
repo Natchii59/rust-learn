8e4a01fb59cc403dc5cee83e9765be9c86ec695f
Natchii59 <caron.nathan@hotmail.com>
2024-02-22 13:52:22 +0100

    clean master

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,1 @@
-mod chap2;
-
-mod chap3_lesson1;
-mod chap3_lesson2;
-mod chap3_lesson3;
-mod chap3_lesson5;
-
-mod chap4_lesson1;
-mod chap4_lesson2;
-mod chap4_lesson3;
-
-mod chap5_lesson1;
-mod chap5_lesson2;
-mod chap5_lesson3;
-
-fn main() {
-    chap5_lesson3::lesson3();
-}
+fn main() {}
