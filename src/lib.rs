1d1ebd661a2d0853f2474fd715fb319161208898
Natchii59 <caron.nathan@hotmail.com>
2024-02-24 10:23:06 +0100

    lesson5

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,7 @@
-use std::collections::*;
-use std::fmt::Result;
-use std::io::{self, Result as IoResult};
+mod front_of_house;
 
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-    }
-}
+pub use crate::front_of_house::hosting;
 
-use front_of_house::hosting::add_to_waitlist;
-
-mod customer {
-    pub fn eat_at_restaurant() {
-        super::add_to_waitlist();
-    }
-}
-
-fn function1() -> Result {
-    Ok(())
-}
-
-fn function2() -> IoResult<()> {
-    Ok(())
+pub fn eat_at_restauranr() {
+    hosting::add_to_waitlist();
 }
